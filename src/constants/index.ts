db03907bed933bdd3704b415aff5abde1973f08c
Christoph Mussenbrock <christoph@etherisc.com>
2021-02-01 01:37:09 +0100

    Deactivate xDai chain, wallets for now

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@uniswap/sdk'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 
-import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
+import { /* fortmatic, */ injected /*, portis, walletconnect, walletlink */ } from '../connectors'
 
 export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
@@ -125,7 +125,7 @@ export const SUPPORTED_WALLETS: { [key: string]: WalletInfo } = {
     description: 'Easy-to-use browser extension.',
     href: null,
     color: '#E8831D'
-  },
+  } /*,
   WALLET_CONNECT: {
     connector: walletconnect,
     name: 'WalletConnect',
@@ -170,6 +170,7 @@ export const SUPPORTED_WALLETS: { [key: string]: WalletInfo } = {
     color: '#4A6C9B',
     mobile: true
   }
+  */
 }
 
 export const NetworkContextName = 'NETWORK'
