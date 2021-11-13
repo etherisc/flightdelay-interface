e0298826a72f0972047f50d1d034b8efb2f638e8
Christoph Mussenbrock <christoph@etherisc.com>
2021-11-13 18:07:17 +0100

    Fix PhishAlert

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -30,16 +30,16 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.uniswap.org</code>
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>fdd-sandbox.etherisc.com</code>
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
-  ) : window.location.hostname === 'app.uniswap.org' ? (
+  ) : ['fdd-demo.etherisc.com', 'fdd-sandbox.etherisc.com'].includes(window.location.hostname) ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Always make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.uniswap.org</code> - bookmark it
-        to be safe.
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>fdd-sandbox.etherisc.com</code> -
+        bookmark it to be safe.
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
