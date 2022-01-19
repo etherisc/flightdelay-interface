48cfd2d1b3cf5090c76fc15f9a62cfe6106cf433
Christoph Mussenbrock <christoph@etherisc.com>
2022-01-19 18:06:48 +0100

    Production Contract address

--- src/hooks/usePurchaseCallback.ts
+++ src/hooks/usePurchaseCallback.ts
@@ -20,7 +20,8 @@ export enum PurchaseCallbackState {
 // const FlightDelayContractAddress = '0xbBe7c65cC7Ad821B0a94eE5CB0B0f9E343e271ae'
 // const FlightDelayContractAddress = '0xF226ba8A04F7Fd15a781bFeD315711299b713dCF' // Product #8 on xDai
 // const FlightDelayContractAddress = '0x9eF34Af6F98dcE7BD5aB07a8b1387d071Ac40f50' // Product #9 on xDai
-const FlightDelayContractAddress = '0x5F69AC65ab461b0cC52f529AA7ac4ac6EE05FE82' // Product #10 on xDai
+// const FlightDelayContractAddress = '0x5F69AC65ab461b0cC52f529AA7ac4ac6EE05FE82' // Product #10 on xDai
+const FlightDelayContractAddress = '0x1d7E2547bb560182B62Dff1EA08D24881A0e057E' // Product #11 on xDai
 
 interface PurchaseParameters {
   /**
